d528547e6219ad456c6a960de199c64a1da86f4d
akt2 <atwitchellheyne@gmail.com>
2019-06-18 22:32:26 -0400

    pandas updating

--- txmay.sql
+++ txmay.sql
@@ -1,7 +1,41 @@
-USE heroku_6e5a2e6badc386d
-CREATE TABLE areas (
-	id INT NOT NULL AUTO_INCREMENT,
-    area INT NOT NULL,
-    area_name VARCHAR(50),
-    PRIMARY KEY (id)
-    );
+USE heroku_6e5a2e6badc386d;
+CREATE TABLE `heroku_6e5a2e6badc386d`.`area` (
+  `id` INT NOT NULL,
+  `code` INT NULL,
+  `name` VARCHAR(45) NULL,
+  `long` INT NULL,
+  `lat` INT NULL,
+  PRIMARY KEY (`id`));
+CREATE TABLE `heroku_6e5a2e6badc386d`.`group` (
+  `id` INT NOT NULL,
+  `group` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+CREATE TABLE `heroku_6e5a2e6badc386d`.`occ` (
+  `id` INT NOT NULL,
+  `code` INT NULL,
+  `name` VARCHAR(45) NULL,
+  `group` INT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `group_idx` (`group` ASC),
+  CONSTRAINT `group`
+    FOREIGN KEY (`group`)
+    REFERENCES `heroku_6e5a2e6badc386d`.`group` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+ALTER TABLE `heroku_6e5a2e6badc386d`.`work` 
+DROP COLUMN `major`,
+CHANGE COLUMN `detail` `occ` INT(11) NULL DEFAULT NULL ,
+ADD INDEX `area_idx` (`area` ASC),
+ADD INDEX `occ_idx` (`occ` ASC);
+;
+ALTER TABLE `heroku_6e5a2e6badc386d`.`work` 
+ADD CONSTRAINT `area`
+  FOREIGN KEY (`area`)
+  REFERENCES `heroku_6e5a2e6badc386d`.`area` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `occ`
+  FOREIGN KEY (`occ`)
+  REFERENCES `heroku_6e5a2e6badc386d`.`occ` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
